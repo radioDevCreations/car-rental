c91c1c52743f074acf5720aa8fc802abd027a579
radioDevCreations <radekrozmus@gmail.com>
2026-03-28 15:13:58 +0100

    add reservation-related classes and components

---
+++ force-app/main/default/lwc/crMyRentals/crMyRentals.js
@@ -0,0 +1,84 @@
+import { LightningElement, track } from 'lwc';
+import getMyReservations from '@salesforce/apex/CR_ReservationController.getMyReservations';
+
+export default class CrMyRentals extends LightningElement {
+    @track reservations = [];
+
+    columns = [
+        {
+            label: 'Name',
+            fieldName: 'Name',
+            type: 'text'
+        },
+        {
+            label: 'Start Date',
+            fieldName: 'StartDate__c',
+            type: 'date'
+        },
+        {
+            label: 'End Date',
+            fieldName: 'EndDate__c',
+            type: 'date'
+        },
+        {
+            label: 'User',
+            fieldName: 'User__c',
+            type: 'text'
+        },
+        {
+            label: 'Vehicle',
+            fieldName: 'Vehicle__c',
+            type: 'text'
+        },
+        {
+            type: 'action',
+            typeAttributes: {
+                rowActions: [
+                    { label: 'View', name: 'view' },
+                    { label: 'Cancel', name: 'cancel' }
+                ]
+            }
+        }
+    ];
+
+    connectedCallback() {
+        this.loadData();
+    }
+
+    loadData() {
+        getMyReservations()
+            .then(result => {
+                this.reservations = result;
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
+
+    handleRowAction(event) {
+        const actionName = event.detail.action.name;
+        const row = event.detail.row;
+
+        switch (actionName) {
+            case 'view':
+                this.handleView(row);
+                break;
+            case 'cancel':
+                this.handleCancel(row);
+                break;
+            default:
+        }
+    }
+
+    handleView(row) {
+        console.log('View:', row);
+
+        // 👉 tu możesz dodać NavigationMixin do przejścia na rekord
+    }
+
+    handleCancel(row) {
+        console.log('Cancel:', row);
+
+        // 👉 tu możesz wywołać Apex do anulowania rezerwacji
+    }
+}
